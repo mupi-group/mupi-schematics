9a3ce349c4685ae7957a1d8cbd49df1cd7e16331
kuku <wuzhao.li@thoughtworks.com>
2021-07-21 21:57:19 +0800

    mupi-release: 0.0.2-alpha.43

--- src/lib/model/files/model/__name__.model.ts
+++ src/lib/model/files/model/__name__.model.ts
@@ -1,1 +1,14 @@
-//
+import {Title, Description, ID} from "@mupi/core";
+
+@Title({
+    title: '<%= name %> management',
+    subtitle: 'config your <%= name %> info here'
+})
+export default class <%= classify(name) %>Model {
+    @ID()
+    @Description('ID')
+    id: string;
+
+    @Description('<%= name %> name')
+    name: string;
+}
