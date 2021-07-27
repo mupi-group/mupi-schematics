b29180b3458d1d0183dbee99ce7792e4de810a7b
kuku <wuzhao.li@thoughtworks.com>
2021-07-27 18:19:12 +0800

    mupi-release: 0.0.2-alpha.59

--- src/lib/express-service/files/aws/type/module/__name__/__name__.resolver.type.ts
+++ src/lib/express-service/files/aws/type/module/__name__/__name__.resolver.type.ts
@@ -14,7 +14,7 @@ export type <%= classify(name) %>Input = {
 };
 
 export type QuerySpecific<%= classify(name) %>Params = {<%= typescriptTypeIDPropertyKey %>: string};
-export type Query<%= classify(name) %>sParams = {<%= name %>Input: <%= classify(name) %>Input, cursor: number, pointer: number};
+export type Query<%= classify(name) %>sParams = {<%= name %>Input: <%= classify(name) %>Input, cursor?: number, pointer?: number};
 export type MutationCreate<%= classify(name) %> = {<%= name %>Input: <%= classify(name) %>Input};
 export type MutationUpdate<%= classify(name) %> = {<%= typescriptTypeIDPropertyKey %>: string, <%= name %>Input: <%= classify(name) %>Input};
 export type MutationDelete<%= classify(name) %> = {<%= typescriptTypeIDPropertyKey %>: string};
